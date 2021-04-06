3085b93703df78a3d9635396c2b37f8dde00aca1
SaurabhSonde <saurabhsonde111@gmail.com>
2021-04-06 12:00:03 +0530

    fixed: custom errors was not return in backend

--- routes/index.js
+++ routes/index.js
@@ -16,7 +16,7 @@ router.get("/:code", async (req, res) => {
     }
   } catch (err) {
     console.error(err);
-    res.status(500).json("Server error");
+    return res.status(500).json({ error: "Server error" });
   }
 });
 
